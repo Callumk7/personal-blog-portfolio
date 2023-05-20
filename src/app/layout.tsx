a4849112621fb41753e66238c0d5e316f686cd4d
Callum Kloos <callumkloos@gmail.com>
2023-05-20 17:07:47 +0100

    added twitter and github to the navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { inter, space_grotesk } from "@/style/fonts";
 export const metadata = {
