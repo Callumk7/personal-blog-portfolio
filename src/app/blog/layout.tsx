def9dd2776d0cebf122b0dc6541b191c1e86c61a
Callum Kloos <callumkloos@gmail.com>
2023-05-22 17:10:23 +0100

    blog post layouts

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -3,11 +3,11 @@ import { getRecentPostFrontmatter } from "@/util/getBlogData";
 
 export default function BlogLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="grid grid-cols-4">
+        <div className="grid grid-cols-4 justify-items-center">
             <div className="col-span-1">
                 <PostSidebar posts={getRecentPostFrontmatter(3)} />
             </div>
-            <div className="col-span-2 justify-items-center">{children}</div>
+            <div className="col-span-3 justify-self-start">{children}</div>
         </div>
     );
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,13 @@
+import PostCard from "@/components/PostCard";
+import { getAllPostFrontmatter, getRecentPostFrontmatter } from "@/util/getBlogData";
+
 export default async function BlogPage() {
+    const posts = getRecentPostFrontmatter(8);
     return (
-        <div>
-            <h1>Blog</h1>
+        <div className="flex flex-wrap">
+            {posts.map((post) => {
+                return <PostCard key={post.slug} post={post} />;
+            })}
         </div>
     );
 }
