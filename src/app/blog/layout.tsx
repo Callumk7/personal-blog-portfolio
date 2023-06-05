e70664d78ee3dfc3e44bc546e1fc73231076eb31
Callum Kloos <callumkloos@gmail.com>
2023-06-05 16:04:13 +0100

    file cleanout

--- src/app/blog/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { HeaderGradientBlog } from "@/components/navbar/HeaderGradient";
-
-export default function BlogLayout({ children }: { children: React.ReactNode }) {
-	return <div>{children}</div>;
-}
