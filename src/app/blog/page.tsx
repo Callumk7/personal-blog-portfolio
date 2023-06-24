17840d326166f4dc3645ca73b3ba7791534946c8
Callum Kloos <callumkloos@gmail.com>
2023-06-24 16:02:57 +0100

    read categories from db

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,14 +4,14 @@ import { getAllCategories } from "@/lib/categories";
 import PostCardView from "@/components/posts/PostCardView";
 
 export default async function BlogPage() {
-  const posts = getRecentPostFrontmatter(8);
-  const categories = await getAllCategories();
+	const posts = getRecentPostFrontmatter(8);
+	const categories = await getAllCategories();
 
-  return (
-    <main className=" mb-24 px-8 md:w-2/3 lg:w-3/4 xl:w-2/3">
-      <TagFilter categories={categories} />
-      <h1 className="pb-10 font-syne text-6xl font-bold text-zinc-900">Blog.</h1>
-      <PostCardView posts={posts} />
-    </main>
-  );
+	return (
+		<main className=" mb-24 px-8 md:w-2/3 lg:w-3/4 xl:w-2/3">
+			<TagFilter categories={categories} />
+			<h1 className="pb-10 font-syne text-6xl font-bold text-zinc-900">Blog.</h1>
+			<PostCardView posts={posts} />
+		</main>
+	);
 }
