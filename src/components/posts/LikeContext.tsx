fb976c87f83ba239f7a633d8e1d6eac737770759
Callum Kloos <callumkloos@gmail.com>
2023-06-28 17:24:11 +0100

    component updates

---
+++ src/components/posts/LikeContext.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { createContext } from "react";
+
+const likeContext = createContext(0);
+
+export function LikeProvider() {
+
+}
--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -1,15 +1,37 @@
-import { PostFrontmatter } from "@/types";
+"use client";
+
 import { Post } from "@prisma/client";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const addLikeOnServer = (id: number) => {
+  fetch(`/api/posts/likes/${id}`, {
+    method: "PATCH"
+  })
+}
 
 export default function PostCard({ post }: { post: Post }) {
-	return (
-		<Link
-			href={`/blog/posts/${post.slug}`}
-			className="flex flex-col border border-black from-primary to-neutral-200 px-2 py-5 transition ease-in hover:bg-primary"
-		>
-			<h2 className="py-3 font-syne text-xl font-bold">{post.title}</h2>
-			<p className="font-figtree pb-2 text-xs">{post.description}</p>
-		</Link>
-	);
+  const [likes, setLikes] = useState(post.likes);
+
+  const handleLike = () => {
+    const newLikes = likes + 1;
+    setLikes(newLikes);
+    addLikeOnServer(post.id);
+  }
+
+  return (
+    <div>
+      <div className="flex flex-row space-x-3">
+        <p className="font-figtree pb-2 text-lg">{likes}</p>
+        <button onClick={handleLike}>like button</button>
+      </div>
+      <Link
+        href={`/blog/posts/${post.slug}`}
+        className="flex flex-col border border-dune-900 px-2 py-5 transition ease-in hover:bg-eminence-700"
+      >
+        <h2 className="py-3 font-syne text-xl font-bold">{post.title}</h2>
+        <p className="font-figtree pb-2 text-xs">{post.description}</p>
+      </Link>
+    </div>
+  );
 }
