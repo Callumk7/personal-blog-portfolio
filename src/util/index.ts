e086c5a384fd80d664719af198e087e82aa83627
Callum Kloos <callumkloos@gmail.com>
2023-06-28 12:26:34 +0100

    utility functions

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,6 +1,24 @@
 import prisma from "@/db/client";
+import { Category, Post } from "@/types";
 
 export async function getAllCategories() {
-	const categories = await prisma.category.findMany();
-	return categories;
+  const categories: Category[] = await prisma.category.findMany();
+  return categories;
+}
+
+export async function getAllPosts() {
+  const posts: Post[] = await prisma.post.findMany();
+  return posts;
+}
+
+export async function incrementLikes(id: number, likes: number) {
+  const update = await prisma.post.update({
+    where: {
+      id: id,
+    },
+    data: {
+      likes: likes,
+    },
+  });
+  return update;
 }
