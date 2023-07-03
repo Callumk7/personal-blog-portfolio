a1f59c36383b301289016d695ae5009f98f0dac6
Callum Kloos <callumkloos@gmail.com>
2023-07-03 17:31:28 +0100

    post component updates

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -1,20 +1,16 @@
+"use client";
+
 import { Post } from "@prisma/client";
 import Link from "next/link";
-import { useState } from "react";
-
-const addLikeOnServer = (id: number) => {
-  fetch(`/api/posts/likes/${id}`, {
-    method: "PATCH",
-  });
-};
 
 export default function PostCard({ post }: { post: Post }) {
   return (
     <div>
       <Link
         href={`/blog/posts/${post.slug}`}
-        className="flex flex-col border border-dune-900 px-2 py-5 transition ease-in hover:bg-eminence-700"
+        className="flex h-52 flex-col overflow-y-hidden border border-dune-900 px-2 py-5 transition ease-in hover:bg-eminence-700"
       >
+        <p className="font-sans text-xs text-dune-400">{post.createdAt.toDateString()}</p>
         <h2 className="py-3 font-syne text-xl font-bold">{post.title}</h2>
         <p className="font-figtree pb-2 text-xs">{post.description}</p>
       </Link>
--- src/components/posts/PostSideBar.tsx
+++ src/components/posts/PostSideBar.tsx
@@ -1,15 +1,33 @@
 import { Post } from "@prisma/client";
+import clsx from "clsx";
+import Link from "next/link";
 
-export default function PostSideBar({ posts }: { posts: Post[] }) {
+export default function PostSideBar({
+  posts,
+  className,
+}: {
+  posts: Post[];
+  className?: string;
+}) {
   return (
-    <div>
-      {posts.map((post) => {
-        return (
-          <div key={post.id}>
-            <p>{post.title}</p>
-          </div>
-        );
-      })}
+    <div className={clsx(className, "border border-dune-900")}>
+      <h2 className="font-syne text-lg font-bold">Related Posts</h2>
+      <ul className="flex flex-col space-y-4">
+        {posts.map((post) => {
+          return (
+            <li key={post.id}>
+              <Link
+                href={`/blog/posts/${post.slug}`}
+                scroll={true}
+                className="text-dune-900 pb-2 hover:text-primary"
+              >
+                {post.title}
+              </Link>
+              <p className="text-sm text-dune-800">{post.createdAt.toDateString()}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
