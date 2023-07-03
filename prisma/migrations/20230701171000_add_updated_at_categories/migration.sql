559444973d08638b65b2553f7d37057f7508addc
Callum Kloos <callumkloos@gmail.com>
2023-07-03 09:17:58 +0100

    db updates

---
+++ prisma/migrations/20230701171000_add_updated_at_categories/migration.sql
@@ -0,0 +1,14 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Category" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "color" TEXT NOT NULL DEFAULT 'none',
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_Category" ("color", "id", "name") SELECT "color", "id", "name" FROM "Category";
+DROP TABLE "Category";
+ALTER TABLE "new_Category" RENAME TO "Category";
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
