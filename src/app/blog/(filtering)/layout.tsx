055180768934c07b4215dd1eb6ef8a08f741d217
Callum Kloos <callumkloos@gmail.com>
2023-07-07 12:56:58 +0100

    blog copy update

--- src/app/blog/(filtering)/layout.tsx
+++ src/app/blog/(filtering)/layout.tsx
@@ -9,11 +9,7 @@ export default async function BlogLayout({ children }: { children: React.ReactNo
     <div className="mt-16">
       <div className="hero flex flex-col items-center border border-slate-400 p-5 md:p-28">
         <Header h={1}>Blog.</Header>
-        <p className="text-center md:w-2/3">
-          Empower your digital creativity with our software development and design blog.
-          Elevate your skills, gather insights, and fuel inspiration. Let&apos;s unlock
-          your full potential!
-        </p>
+        <p className="text-center md:w-2/3">I post about anything, but mostly web development, and what I have learn&apos;t along the way. Presently you can enjoy posts written by ChatGPT to test my designs. Have fun!</p>
       </div>
       <TagFilter categories={categories} />
       {children}
